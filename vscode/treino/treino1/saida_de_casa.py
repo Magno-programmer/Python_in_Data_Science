a2c28445f28990b603fc2fb9113066d91e6d662a
Carlos magno <83795938+Magno-programmer@users.noreply.github.com>
2022-09-16 11:59:28 -0300

    adicionando comentários

--- vscode/treino/treino1/saida_de_casa.py
+++ vscode/treino/treino1/saida_de_casa.py
@@ -1,19 +1,24 @@
+# Forma de praticar meu python para data science
 from random import randrange
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt # Importando biblioteca
 
 meses_do_ano = ['Janeiro', 'Fevereiro', 'Março',
                 'Abril', 'Maio', 'Junho', 
                 'Julho', 'Agosto', 'Setembro', 
                 'Outubro', 'Novembro', 'Dezembro']
 
+# Lista para receber as vezes que poderei sair por mês
 vezes_que_posso_sair = []
 
+# Looping para quantia de mêses
 for x in range(12):
-    vezes_que_posso_sair.append(randrange(0, 5))
+    # atribuindo uma quantidade aleatória por mes de 0 a 4.
+    vezes_que_posso_sair.append(randrange(0, 5)) 
     
 x = meses_do_ano
 y = vezes_que_posso_sair
 
+# Configurações para plotar a tabela
 plt.figure(figsize=(12, 4))
 
 plt.plot(x, y, marker='o')
