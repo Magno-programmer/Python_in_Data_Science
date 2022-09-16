ee4722c88bbf66d0f3d0e27a228c13b2dd7d7f88
Carlos magno <83795938+Magno-programmer@users.noreply.github.com>
2022-09-16 11:53:46 -0300

    tabela aleatoria para saídas no ano por mês

--- vscode/treino/treino1/saida_de_casa.py
+++ vscode/treino/treino1/saida_de_casa.py
@@ -1,4 +1,23 @@
 from random import randrange
 import matplotlib.pyplot as plt
 
+meses_do_ano = ['Janeiro', 'Fevereiro', 'Março',
+                'Abril', 'Maio', 'Junho', 
+                'Julho', 'Agosto', 'Setembro', 
+                'Outubro', 'Novembro', 'Dezembro']
 
+vezes_que_posso_sair = []
+
+for x in range(12):
+    vezes_que_posso_sair.append(randrange(0, 5))
+    
+x = meses_do_ano
+y = vezes_que_posso_sair
+
+plt.figure(figsize=(12, 4))
+
+plt.plot(x, y, marker='o')
+plt.title('Quantidade permitida para sair')
+plt.yticks(range(0, 5))
+plt.grid()
+plt.show()
