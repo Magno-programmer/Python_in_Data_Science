3d8bb2048e3033904e1458be5809e9bc68eb015f
Carlos magno <83795938+Magno-programmer@users.noreply.github.com>
2022-09-24 02:49:43 -0300

    alterações de testes na lista

--- pycharm/treino/treino4/listas.py
+++ pycharm/treino/treino4/listas.py
@@ -1,18 +1,41 @@
 # names
-names = ["carlos", "lucas", "gustavo", "alice", "joao", "andre", "augusto"]
+names = ["carlos", "luciana", "gustavo", "alice", "joao",
+         "andre", "augusto", "jorge", "heloisa", "camila",
+         "george", "anderson", "benir", "benjamin", "paula",
+         "leo", "lucas", "jersica", "aparecida", "maria"]
 
 """ tests """
 x = "carol"
 t = "joaquim"
 h = "jo"
-n = '3'
+n = 'hugo'
 
+# show one element in list
+# show = names[3]
+# print(show)
+
+# change the element j in list to x
+#       j
 # names[3] = x
 # print(names[:])
 
+# show the element i until j in list except j
+#                i j
+# show_2 = names[1:3]
+# print(show_2)
+
+# change the element i until j in list to h, except j
+#       i j
 # names[1:3] = h
 # print(names[:])
 
-names[0:4:2] = n
-print(names[:])
+
+'''
+ change the element i until j jumping k elements in list for each element n
+ if the difference from i through j equal length of n
+'''
+#       i j k
+# names[2:6:1] = n # --> n = 4 length, i=2 and j=6, i-j=4
+# print(names[:])
+
 
