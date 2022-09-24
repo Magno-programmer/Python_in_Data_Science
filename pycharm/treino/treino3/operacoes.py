f85edbec2ba99de470d3799b8603c48484f266ca
Carlos magno <83795938+Magno-programmer@users.noreply.github.com>
2022-09-24 01:03:15 -0300

    listas e operações adicionados

---
+++ pycharm/treino/treino3/operacoes.py
@@ -0,0 +1,49 @@
+"""
+# sum (+)
+sum = 2+2
+print(sum, " sum")
+# output = 4
+
+# subtract (-)
+subtract = 2-2
+print(subtract, " subtract")
+# output = 0
+
+# multiplication (*)
+multiplication = 2*3
+print(multiplication, " multiplication")
+# output = 6
+
+# potentiate (**)
+potentiate = 2**3
+print(potentiate, " potentiate")
+# output = 8
+
+# division real (/)
+division_result_float = 2 / 3
+print(division_result_float, " real division")
+# output = 0.666666...
+
+# division integrate (//)
+division_result_int = 2 // 3
+print(division_result_int, " integrate division")
+# output = 0
+
+# rest of division (%)
+rest_of_division = 3 % 2
+print(rest_of_division, " rest of division")
+# output = 1
+"""
+
+print("*"*5, "Exercises", "*"*5)
+
+alternative_one = 10 % 2 + 3 // 10
+print("1) 10 % 2 + 3 // 10 = ", alternative_one)
+
+alternative_two = 5 * (2 + 3) / 2
+print("2) 5 * (2 + 3) / 2 = ", alternative_two)
+
+alternative_three = 2 ** 3 * 4
+print("3) 2 ** 3 * 4 = ", alternative_three)
+
+
---
+++ pycharm/treino/treino4/listas.py
@@ -0,0 +1,18 @@
+# names
+names = ["carlos", "lucas", "gustavo", "alice", "joao", "andre", "augusto"]
+
+""" tests """
+x = "carol"
+t = "joaquim"
+h = "jo"
+n = '3'
+
+# names[3] = x
+# print(names[:])
+
+# names[1:3] = h
+# print(names[:])
+
+names[0:4:2] = n
+print(names[:])
+
