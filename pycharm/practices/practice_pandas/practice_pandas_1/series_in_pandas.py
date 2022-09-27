38bb6c7d0f0cf53bedaaaea255d7122560728f27
Carlos magno <83795938+Magno-programmer@users.noreply.github.com>
2022-09-27 15:47:25 -0300

    Starting library pandas

---
+++ pycharm/practices/practice_pandas/practice_pandas_1/series_in_pandas.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+name_cars = ["Gol", "Chevrolet", "Fiat Uno", "Palio"]
+
+
+"""               A Series in library pandas               """
+
+# In a series the output will be the order of the list in columns
+# and for each index going to exist a value
+print(pd.Series(name_cars))
