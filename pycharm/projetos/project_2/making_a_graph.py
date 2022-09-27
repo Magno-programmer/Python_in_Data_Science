2151b70cbdc845fd5253f80f8ce969ee5e011f22
Carlos magno <83795938+Magno-programmer@users.noreply.github.com>
2022-09-27 01:05:23 -0300

    Putting what I learn in my project

--- pycharm/projetos/project_2/making_a_graph.py
+++ pycharm/projetos/project_2/making_a_graph.py
@@ -6,9 +6,7 @@ years_cars = np.loadtxt('cars/cars-years.txt', dtype=int)
 km_cars = np.loadtxt('cars/cars-km.txt', dtype=float)
 zero_km_cars = np.loadtxt('cars/cars-zero-km.txt', dtype=int)
 
-info_cars = (names_cars.tolist() + price_cars.tolist() + years_cars.tolist() + km_cars.tolist())
-info_graph = np.array(info_cars).reshape((258, 4), order='F')
-
+info_graph = np.column_stack((names_cars, price_cars, years_cars, km_cars, zero_km_cars))
 print(info_graph)
 
 
